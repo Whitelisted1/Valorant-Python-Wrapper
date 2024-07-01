e932181fa26d3fab03d22abf1ec0160008aad73a
Whitelisted <77711834+Whitelisted1@users.noreply.github.com>
2024-06-30 21:36:01 -0600

    Move items from old match.py to new match.py

--- src/valorant/match/match.py
+++ src/valorant/match/match.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING
+import requests
 
 if TYPE_CHECKING:
     from ..session import Session
@@ -6,13 +7,31 @@ if TYPE_CHECKING:
 
 
 class Match:
-    def __init__(self, session: "Session", matchID: str, mapID: str = None, modeID: str = None, players: "Players" = None):
+    def __init__(self, session: "Session", matchID: str, mapID: str = None, modeID: str = None, players: "Players" = None, shard: str = "na", region: str = "na",):
+        self.session = session
+
         self.matchID = matchID
         self.map = mapID
         self.mode = modeID
 
+        self.pd_url = f"https://pd.{shard}.a.pvp.net"
+        self.glz_url = f"https://glz-{shard}-1.{region}.a.pvp.net"
+
+        self.shard = shard
+        self.region = region
+
         if players is None: self.players = Players(session)
         else: self.players: Players = players
+
+    def get_match_data_raw(self) -> dict:
+        """
+        Fetches match data for this object
+
+        Returns:
+        dict: The JSON data which represents this Match's data
+        """
+
+        return self.session.fetch(f"{self.pd_url}/match-details/v1/matches/{self.matchID}")
 
     @staticmethod
     def from_json(session: "Session", data: dict) -> "Match":
