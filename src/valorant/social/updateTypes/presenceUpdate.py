beab7a20622b3a5cef2cbf9f3395ec67d859afa0
Whitelisted <77711834+Whitelisted1@users.noreply.github.com>
2024-07-07 19:15:36 -0600

    Change PresenceUpdate to recognize when player goes offline

--- src/valorant/social/updateTypes/presenceUpdate.py
+++ src/valorant/social/updateTypes/presenceUpdate.py
@@ -15,25 +15,23 @@ class PresenceUpdate:
     def __init__(self, session: "Session", XML_data: ET.Element):
         self.raw = XML_data
 
-        self.invalid = False
+        self.from_user = User(session, XML_data.attrib['from'].split("@")[0])
+        self.to_user = User(session, XML_data.attrib['to'].split("@")[0])
+        self.is_self = self.from_user.puuid == self.to_user.puuid
 
         self.presence_data = XML_data.find('.//p')
 
-        if self.presence_data is None:
-            self.invalid = True
+        self.is_unavailable = "type" in XML_data.attrib and XML_data.attrib["type"] == "unavailable"
+        self.invalid = self.presence_data is None
+
+        if self.is_unavailable or self.invalid:
             return
 
         self.presence_data = json.loads(base64.b64decode(self.presence_data.text).decode())
 
-        self.from_user = User(
-            session,
-            XML_data.attrib['from'].split("@")[0],
-            rank=Rank(self.presence_data["competitiveTier"], None),
-            account_level=self.presence_data["accountLevel"]
-        )
-        self.to_user = User(session, XML_data.attrib['to'].split("@")[0])
+        self.from_user.rank = Rank(self.presence_data["competitiveTier"], None)
+        self.from_user.account_level = self.presence_data["accountLevel"]
 
-        self.is_self = self.from_user.puuid == self.to_user.puuid
         self.is_idle = self.presence_data["isIdle"]
 
         self.queueID = self.presence_data["queueId"]
